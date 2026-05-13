6e28af4ac413a569d280558d9c64db574dbaceec
paras898 <prs89826@gmail.com>
2026-05-13 13:41:03 +0530

    Adding BE code

---
+++ src/main/java/com/legacybridge/WiproApplication.java
@@ -0,0 +1,11 @@
+package com.legacybridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiproApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WiproApplication.class, args);
+    }
+}
